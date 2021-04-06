caf02974266a545dc42065303bc5b200c5efa4d0
Philipp Wolff <pwwolff@gmail.com>
2021-04-07 01:27:10 +0200

    Cargo example

---
+++ hello_cargo/target/debug/deps/hello_cargo-45d37fc39133ed4f.d
@@ -0,0 +1,5 @@
+/home/philipp/dev/rust_book/hello_cargo/target/debug/deps/hello_cargo-45d37fc39133ed4f: src/main.rs
+
+/home/philipp/dev/rust_book/hello_cargo/target/debug/deps/hello_cargo-45d37fc39133ed4f.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/philipp/dev/rust_book/hello_cargo/target/debug/hello_cargo: /home/philipp/dev/rust_book/hello_cargo/src/main.rs
